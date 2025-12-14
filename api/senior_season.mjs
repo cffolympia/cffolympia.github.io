ac1573c917b6a551948d45abe7c102e8a29b3717
micezer <obiezeh99@gmail.com>
2025-12-14 19:59:37 +0100

    Resultado contra Extremadura

--- api/senior_season.mjs
+++ api/senior_season.mjs
@@ -159,7 +159,7 @@ let SEASON_SENIOR_A = [
     date: "Dec 14, 2025 17:00:00",
     shortDate: "14/12/25",
     isLocal: true,
-    result: "-",
+    result: "1-0",
     mapsURL: "https://maps.app.goo.gl/d2WLLm51F5Qv8iQJA",
     liveURL: "",
     hashtag: "#OLYMPIAEXTREMADURA",
